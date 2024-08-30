df6af5d7f1b3f2509622423115ec23e31e71716b
Ranjan Mishra <mishra.ranjan5@gmail.com>
2024-08-30 16:38:25 +0530

    Update user.service.ts logger

--- auth-server/src/user/user.service.ts
+++ auth-server/src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectConnection, InjectModel } from '@nestjs/mongoose';
 import { Connection, Model } from 'mongoose';
 import * as bcrypt from 'bcrypt';
@@ -7,6 +7,8 @@ import { CreateUserDto } from './dto/user.dto';
 
 @Injectable()
 export class UserService {
+  private readonly logger = new Logger(UserService.name);
+
   constructor(
     @InjectModel(User.name) private userModel: Model<UserDocument>,
     @InjectConnection() private connection: Connection
@@ -14,21 +16,28 @@ export class UserService {
   ) {}
 
   async create(createUserDto: CreateUserDto): Promise<UserDocument> {
+    this.logger.log(`Creating user with email: ${createUserDto.email}`);
     const { name, email, password } = createUserDto;
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = new this.userModel({ name, email, password: hashedPassword });
+    this.logger.log(`User created with ID: ${user._id.toString()}`);
+
     return user.save();
   }
 
   async validateUser(email: string, password: string): Promise<User | null> {
+    this.logger.log(`Validating user with email: ${email}`);
     const user = await this.userModel.findOne({ email });
     if (user && (await bcrypt.compare(password, user.password))) {
+      this.logger.log(`User validated successfully: ${user._id}`);
       return user;
     }
+    this.logger.warn(`User validation failed for email: ${email}`);
     return null;
   }
 
   async findByEmail(email: string): Promise<User | null> {
+    this.logger.log(`Finding user by email: ${email}`);
     return this.userModel.findOne({ email });
   }
 }
